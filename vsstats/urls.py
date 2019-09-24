24d2ee6ea49893bdb428852b0b77b1f9f851dbfc
AliRzaev <alirzaev1996@gmail.com>
2019-09-24 11:40:23 +0300

    Fix bug with url namespaces

--- vsstats/urls.py
+++ vsstats/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'vsstats'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('sources', views.index, name='index'),
